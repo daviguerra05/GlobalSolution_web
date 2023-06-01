adc5efd4e7e08c516b27400bfdd52a5d1cfdef94
daviguerra05 <rm551605@fiap.com.br>
2023-06-01 20:56:42 -0300

    toggle bt

--- scripts/principal.js
+++ scripts/principal.js
@@ -1,3 +1,6 @@
+const toggle = document.querySelector('#toggle')
+const fome = document.querySelector('#fome')
+
 function mudar(valorx){
     var texto = document.getElementById('fome_divider')
     valorx = (valorx - 2000)/40;
@@ -7,5 +10,16 @@ function mudar(valorx){
 document.addEventListener('mousemove', (evento) => {
     const mouseX = evento.clientX;
     mudar(mouseX)
-    console.log(mouseX)
+    // console.log(mouseX)
 })
+
+toggle.addEventListener('click',(e)=>{
+    var is_toggle = toggle.checked
+    if(!is_toggle == true){
+        fome.style.background = 'black'
+    }
+    else{
+        fome.style.background = 'white'
+    }
+})
+    
