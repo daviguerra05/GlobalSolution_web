4ffa69811e25d1b2a2d10d467147ae81aae6a7ad
daviguerra05 <rm551605@fiap.com.br>
2023-06-06 23:19:23 -0300

    images replace

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,9 @@
 window.addEventListener('load', (loaded)=>{
     alert('Seja bem vindo')
 })
+
+var button = document.getElementById('button')
+
+button.onclick = function(){
+    window.location.href = './pages/principal.html'
+}
