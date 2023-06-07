612dd31d0b91d878f8e8ac65e88fa4ea9dd77b79
daviguerra05 <rm551605@fiap.com.br>
2023-06-06 23:58:17 -0300

    time and date function

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,14 @@ var button = document.getElementById('button')
 button.onclick = function(){
     window.location.href = './pages/principal.html'
 }
+
+let data = document.getElementById('data')
+let time = document.getElementById('time')
+
+
+setInterval((e)=>{
+    let d = new Date()
+    data.innerHTML = d.toLocaleDateString()
+    time.innerHTML = d.toLocaleTimeString()
+},1000)
+
